34f16428d8cc97f195997442fade20dbe05820c9
asforest <asforest@qq.com>
2022-01-07 22:55:47 +0800

    refactor: adjust default columns and rows of preset to 80 x 25

--- src/main/kotlin/model/EnvironmentalPreset.kt
+++ src/main/kotlin/model/EnvironmentalPreset.kt
@@ -8,8 +8,8 @@ data class EnvironmentalPreset(
     var env: HashMap<String, String> = HashMap(),
     var input: String = "",
     var singleInstance: Boolean = false,
-    var columns: Int = 256,
-    var rows: Int = 64
+    var columns: Int = 80,
+    var rows: Int = 25
 ) {
     override fun toString(): String
     {
