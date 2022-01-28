96654cd8013e8af019a02678b5148db17b877b96
asforest <asforest@qq.com>
2022-01-28 22:35:46 +0800

    chore(ci): optimize Gradle task to build artificial

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,16 @@ val gitCommitSha: String? get() = System.getenv("GITHUB_SHA") ?: null
 val timestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z").format(Date()) as String
 
 group = "com.github.asforest"
-version = gitTagName?.run { getVersionName(this) } ?: "0.0.0"
+version = gitTagName?.run { getVersionName(this) } ?: System.getenv("VERSION") ?: "0.0.0"
 
 plugins {
     val kotlinVersion = "1.6.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-
     id("net.mamoe.mirai-console") version "2.9.0"
 }
 
 repositories {
-//    maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
     maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
 }
@@ -36,33 +34,41 @@ tasks.withType<JavaCompile> {
     targetCompatibility = "11"
 }
 
-tasks.register("buildWithManifest") {
-    dependsOn(tasks.named("buildPlugin"))
 
-    tasks.named<BuildMiraiPluginTask>("buildPlugin").get().apply {
+tasks.whenTaskAdded {
+    if(this is BuildMiraiPluginTask && name == "buildPlugin")
+    {
+        // 在manifest里添加信息
         manifest {
             attributes("Mirai-Plugin-Version" to archiveVersion.get())
             attributes("Git-Commit" to (gitCommitSha ?: ""))
             attributes("Compile-Time" to timestamp)
             attributes("Compile-Time-Ms" to System.currentTimeMillis())
         }
-    }
-}
 
-tasks.register("developing", Copy::class) {
-    dependsOn(tasks.named("buildWithManifest"))
+        // 打包源代码
+        sourceSets.main.get().allSource.sourceDirectories.map {
+            if(it.name != "resources")
+                from(it) {into("sources/"+it.name) }
+        }
 
-    val archive = project.buildDir.path+File.separator+"mirai"+
-            File.separator+project.name+"-"+version+".mirai.jar"
+        // 注册开发任务
+        tasks.register("develop", Copy::class) {
+            dependsOn(tasks.named("buildPlugin"))
 
-    val env = System.getenv()["PluginDebugDir"]
-        ?: throw RuntimeException("The environmental variable 'PluginDebugDir' is not set")
-    if(!File(env).run { !exists() || isDirectory })
-        throw RuntimeException("The 'PluginDebugDir' $env does not exist or is a file")
+            val archive = project.buildDir.path + File.separator + "mirai" + File.separator + project.name + "-" + archiveVersion.get() + ".mirai.jar"
 
-    from(archive).into(env).doLast {
-        val cps = env+File.separator+"cp.bat"
-        if(File(cps).exists())
-            Runtime.getRuntime().exec(cps)
+            val outputPath = System.getenv()["OutputDir"]?.replace("/", "\\")
+            val outputDir = outputPath
+                ?.run { File(this) }
+                ?.run { if(!exists() || !isDirectory) null else this }
+
+            if(outputDir != null)
+            {
+//                Runtime.getRuntime().exec("cmd /C \"copy /Y $archive $outputPath\"")
+                from(archive).into(outputPath)
+                println("Copied $archive -> $outputPath")
+            }
+        }
     }
 }
