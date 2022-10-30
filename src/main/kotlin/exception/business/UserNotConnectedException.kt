5ce5d3676d9731f6ef8a8a7dadb8b7a509f1bb00
asforest <asforest@qq.com>
2022-10-31 01:25:16 +0800

    refactor: modify tip message fo UserNotConnectedException

--- src/main/kotlin/exception/business/UserNotConnectedException.kt
+++ src/main/kotlin/exception/business/UserNotConnectedException.kt
@@ -5,7 +5,7 @@ import com.github.asforest.mshell.session.SessionUser
 
 class UserNotConnectedException : AbstractBusinessException
 {
-    constructor() : super("当前并未连接到当前会话上")
+    constructor() : super("当前并未连接到任何会话上")
 
     constructor(group: SessionUser.GroupUser) : super("$group 还未连接到当前会话上")
 }
